0340d95af78fa223b4922d393e59fa84e27fbaf0
ButakovIgor <t4w3rr@gmail.com>
2023-10-14 06:11:47 +0500

    Task3

---
+++ Tyuiu.ButakovIK.Sprint2.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ButakovIK.Sprint2.Task3.V10.Lib;
+
+namespace Tyuiu.ButakovIK.Sprint2.Task3.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double y = ds.Calculate(2);
+            Assert.AreEqual(-70.25, y);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double y = ds.Calculate(0);
+            Assert.AreEqual(-1, y);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double y = ds.Calculate(-2);
+            Assert.AreEqual(0.036, y);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double y = ds.Calculate(-15);
+            Assert.AreEqual(-164.933, y);
+        }
+    }
+}
---
+++ Tyuiu.ButakovIK.Sprint2.Task3.V10/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ButakovIK.Sprint2.Task3.V10.Lib;
+
+namespace Tyuiu.ButakovIK.Sprint2.Task3.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Бутаков И.К. | АСОиУБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Таск #3                                                                 *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Бутаков Игорь Константинович | АСОиУБ-23-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь вводит    *");
+            Console.WriteLine("* значение переменной X с клавиатуры.                                     *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой.             *");
+            Console.WriteLine("*          .                                                              *");
+            Console.WriteLine("*         |                      x                                        *");
+            Console.WriteLine("*         |x - ( x + 15 / x - 4 )  , если x > 0                           *");
+            Console.WriteLine("*         |                                                               *");
+            Console.WriteLine("*         | 2        2         2        2                                 *");
+            Console.WriteLine("*         |x + cos( X ) - 3 / x - sin( X ) + 2, если x = 0                *");
+            Console.WriteLine("*    y = <                                                                *");
+            Console.WriteLine("*         |           2 2                                                 *");
+            Console.WriteLine("*         |( 5 + 1 / x ), если -14 < x < 0                                *");
+            Console.WriteLine("*         |                                                               *");
+            Console.WriteLine("*         |x + 10x - (1/x), если x < -14                                  *");
+            Console.WriteLine("*         |                                                               *");
+            Console.WriteLine("*          '                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double y = ds.Calculate(x);
+            Console.WriteLine(y);
+            Console.ReadKey();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
